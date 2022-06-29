f22694ea70534f44ef1d6508c3504dd9ea2ac71e
Jakob Borg <jakob@kastelo.net>
2022-06-29 09:20:29 +0200

    wip

--- lib/fs/osdata.go
+++ lib/fs/osdata.go
@@ -4,8 +4,6 @@ import (
 	"github.com/syncthing/syncthing/lib/protocol"
 )
 
-var osDataSetter = NewOSDataSetter()
-
 type OSDataSetter interface {
 	// SetOSData sets the operating system private data for the current
 	// operating system onto the destination FileInfo, and leaves privata
--- lib/fs/osdata_windows.go
+++ lib/fs/osdata_windows.go
@@ -2,11 +2,40 @@ package fs
 
 import (
 	"github.com/syncthing/syncthing/lib/protocol"
+	"golang.org/x/sys/windows"
 )
+
+func NewOSDataSetter() OSDataSetter {
+	return &WindowsOSDataSetter{}
+}
 
 type WindowsOSDataSetter struct {
 }
 
 func (p *WindowsOSDataSetter) SetOSData(dst *protocol.FileInfo, fi FileInfo) error {
+	hdl, err := windows.Open(dst.Name, windows.O_RDONLY, 0)
+	if err != nil {
+		return err
+	}
+	defer windows.Close(hdl)
+
+	sd, err := windows.GetSecurityInfo(hdl, windows.SE_FILE_OBJECT, windows.DACL_SECURITY_INFORMATION)
+	if err != nil {
+		return err
+	}
+	owner, _, err := sd.Owner()
+	if err != nil {
+		return err
+	}
+
+	pd := &protocol.WindowsPrivateData{
+		OwnerSid: owner.String(),
+	}
+	bs, _ := pd.Marshal()
+	if dst.OsPrivateData == nil {
+		dst.OsPrivateData = make(map[protocol.OS][]byte)
+	}
+	dst.OsPrivateData[protocol.OsWindows] = bs
+
 	return nil
 }
