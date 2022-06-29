01aec4d98075c2bc9b545a11e8eae06520e1e6cb
Jakob Borg <jakob@kastelo.net>
2022-06-29 10:27:52 +0200

    wip

--- lib/fs/osdata_posix.go
+++ lib/fs/osdata_posix.go
@@ -1,54 +1,37 @@
+// Copyright (C) 2022 The Syncthing Authors.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
 package fs
 
 import (
 	"fmt"
 	"os/user"
 	"strconv"
-	"sync"
-	"time"
 
 	"github.com/syncthing/syncthing/lib/protocol"
 )
 
-const (
-	positiveNameCachetime = time.Hour
-	negativeNameCachetime = 5 * time.Minute
-)
-
 func NewPOSIXDataGetter(_ Filesystem) OSDataGetter {
-	return &POSIXOSDataGetter{
-		users:  newNameCache(),
-		groups: newNameCache(),
-	}
+	return &POSIXOSDataGetter{}
 }
 
-type POSIXOSDataGetter struct {
-	users  *nameCache
-	groups *nameCache
-}
+type POSIXOSDataGetter struct{}
 
 func (p *POSIXOSDataGetter) GetOSData(_ *protocol.FileInfo, stat FileInfo) (map[protocol.OS][]byte, error) {
-	// Look up owner and group ID and names via the cache. We swallow errors
-	// in the name lookup, because the end result is anyway the empty
-	// string.
-
 	ownerUID := stat.Owner()
-	ownerName, _ := p.users.getOrPopulate(ownerUID, func(uid int) (string, error) {
-		user, err := user.LookupId(strconv.Itoa(uid))
-		if err != nil {
-			return "", err
-		}
-		return user.Username, nil
-	})
+	ownerName := ""
+	if u, err := user.LookupId(strconv.Itoa(ownerUID)); err == nil {
+		ownerName = u.Username
+	}
 
 	groupID := stat.Group()
-	groupName, _ := p.users.getOrPopulate(groupID, func(gid int) (string, error) {
-		group, err := user.LookupGroupId(strconv.Itoa(gid))
-		if err != nil {
-			return "", err
-		}
-		return group.Name, nil
-	})
+	groupName := ""
+	if g, err := user.LookupGroupId(strconv.Itoa(groupID)); err == nil {
+		groupName = g.Name
+	}
 
 	// Create the POSIX private data structure and store it marshalled.
 	pd := &protocol.POSIXOSData{
@@ -64,57 +47,4 @@ func (p *POSIXOSDataGetter) GetOSData(_ *protocol.FileInfo, stat FileInfo) (map[
 	return map[protocol.OS][]byte{
 		protocol.OsPosix: bs,
 	}, nil
-}
-
-type nameCache struct {
-	names map[int]nameCacheEntry
-	mut   sync.RWMutex
-}
-
-func newNameCache() *nameCache {
-	return &nameCache{
-		names: make(map[int]nameCacheEntry),
-	}
-}
-
-type nameCacheEntry struct {
-	name string
-	err  error
-	when time.Time
-}
-
-func (cache *nameCache) getOrPopulate(id int, get func(int) (string, error)) (string, error) {
-	// In the best case we'll have a cache hit, optimize for that case by
-	// taking a read lock only and for a short while.
-	now := time.Now()
-	cache.mut.RLock()
-	entry, ok := cache.names[id]
-	cache.mut.RUnlock()
-	switch {
-	case ok && entry.err == nil && now.Sub(entry.when) < positiveNameCachetime:
-		return entry.name, nil
-	case ok && entry.err != nil && now.Sub(entry.when) < negativeNameCachetime:
-		return "", entry.err
-	}
-
-	// We didn't, so take a write lock and populate the cache.
-	cache.mut.Lock()
-	defer cache.mut.Unlock()
-	// Someone might still have made this before we did, so check again
-	// before doing the heavy work.
-	entry, ok = cache.names[id]
-	switch {
-	case ok && entry.err == nil && now.Sub(entry.when) < positiveNameCachetime:
-		return entry.name, nil
-	case ok && entry.err != nil && now.Sub(entry.when) < negativeNameCachetime:
-		return "", entry.err
-	}
-
-	name, err := get(id)
-	if err != nil {
-		cache.names[id] = nameCacheEntry{err: err, when: now}
-		return "", err
-	}
-	cache.names[id] = nameCacheEntry{name: name, when: now}
-	return name, nil
 }
